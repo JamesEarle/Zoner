a678ef4b9edd386198efdf67e0f38f08d625766a
James Earle <j_earle@hotmail.com>
2015-09-06 02:09:44 -0400

    map integration, mixed with Laravel

--- cbh/resources/views/welcome.blade.php
+++ cbh/resources/views/welcome.blade.php
@@ -1,45 +1,130 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends('app')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+@section('content')
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+<style>
+    /*html, body {
+        height: 100%;
+    }
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+    body {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        display: table;
+        font-weight: 100;
+        font-family: 'Lato';
+    }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+    .container {
+        text-align: center;
+        display: table-cell;
+        vertical-align: middle;
+    }
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+    .content {
+        text-align: center;
+        display: inline-block;
+    }
+*/
+    .title {
+        font-size: 96px;
+    }
+</style>
+<div class="container">
+    <div class="geo-location-wrapper">
+        <span class="btn geo-location"><i class="fa fa-map-marker"></i><span class="text">Find My Position</span></span>
+    </div>
+</div>
 
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
+<!-- Map -->
+<div id="map" class="has-parallax"></div>
+<!-- end Map -->
+
+<!-- Search Box -->
+<div class="search-box-wrapper">
+    <div class="search-box-inner">
         <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+            <div class="row">
+                <div class="col-md-3 col-sm-4">
+                    <div class="search-box map">
+                        <form role="form" id="form-map" class="form-map form-search">
+                            <h2>Search Your Property</h2>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="search-box-property-id" placeholder="Property ID">
+                            </div>
+                            <div class="form-group">
+                                <select name="type">
+                                    <option value="">Status</option>
+                                    <option value="1">Rent</option>
+                                    <option value="2">Sale</option>
+                                </select>
+                            </div><!-- /.form-group -->
+                            <div class="form-group">
+                                <select name="country">
+                                    <option value="">Country</option>
+                                    <option value="1">France</option>
+                                    <option value="2">Great Britain</option>
+                                    <option value="3">Spain</option>
+                                    <option value="4">Russia</option>
+                                    <option value="5">United States</option>
+                                </select>
+                            </div><!-- /.form-group -->
+                            <div class="form-group">
+                                <select name="city">
+                                    <option value="">City</option>
+                                    <option value="1">New York</option>
+                                    <option value="2">Los Angeles</option>
+                                    <option value="3">Chicago</option>
+                                    <option value="4">Houston</option>
+                                    <option value="5">Philadelphia</option>
+                                </select>
+                            </div><!-- /.form-group -->
+                            <div class="form-group">
+                                <select name="district">
+                                    <option value="">District</option>
+                                    <option value="1">Manhattan</option>
+                                    <option value="2">The Bronx</option>
+                                    <option value="3">Brooklyn</option>
+                                    <option value="4">Queens</option>
+                                    <option value="5">Staten Island</option>
+                                </select>
+                            </div><!-- /.form-group -->
+                            <div class="form-group">
+                                <select name="property-type">
+                                    <option value="">Property Type</option>
+                                    <option value="1">Apartment</option>
+                                    <option value="2">Condominium</option>
+                                    <option value="3">Cottage</option>
+                                    <option value="4">Flat</option>
+                                    <option value="5">House</option>
+                                </select>
+                            </div><!-- /.form-group -->
+                            <div class="form-group">
+                                <div class="price-range">
+                                    <input id="price-input" type="text" name="price" value="1000;299000">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-default">Search Now</button>
+                            </div><!-- /.form-group -->
+                        </form><!-- /#form-map -->
+                    </div><!-- /.search-box.map -->
+                </div><!-- /.col-md-3 -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.search-box-inner -->
+</div>
+<!-- end Search Box -->
+
+<!-- Page Content -->
+<div id="page-content">
+    <section id="banner">
+        <div class="block has-dark-background background-color-default-darker center text-banner">
+            <div class="container">
+                <h1 class="no-bottom-margin no-border">Zoner Is Fully Loaded Real Estate Template with <a href="#" class="text-underline">Tons of Features</a>!</h1>
             </div>
         </div>
-    </body>
-</html>
+    </section><!-- /#banner -->
+</div>
+@endsection
