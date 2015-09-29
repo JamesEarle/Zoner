08e1cf9d2428ba0b36181b88a8e50ba7cf2c8ece
James Earle <j_earle@hotmail.com>
2015-09-29 12:02:06 -0400

    adjusted for case sensitivity in linux

--- cbh/app/queries.php
+++ cbh/app/queries.php
@@ -1,9 +1,9 @@
 <?php
 //Query constants to be used globally.
 
-define("ALL_LOCATIONS", "SELECT * FROM LOCATIONS");
+define("ALL_LOCATIONS", "SELECT * FROM `locations`");
 
-define("LOCATIONS_BY_PROVINCE", "SELECT * FROM LOCATIONS WHERE PROVINCE=");
+define("LOCATIONS_BY_PROVINCE", "SELECT * FROM `locations` WHERE `province`=");
 
 define("INSERT_INTO_USERS", "INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`) ");
 
--- cbh/database/migrations/2015_09_29_035529_create_landlords_table.php
+++ cbh/database/migrations/2015_09_29_035529_create_landlords_table.php
@@ -29,6 +29,6 @@ class CreateLandlordsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('locations');
+        Schema::drop('landlords');
     }
 }
