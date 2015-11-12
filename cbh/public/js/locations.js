428352242f32af8a8206b5daff1e990164194831
James Earle <j_earle@hotmail.com>
2015-11-12 11:43:12 -0500

    commenting out php, undefined index 0 error

--- cbh/public/js/locations.js
+++ cbh/public/js/locations.js
@@ -4,7 +4,5 @@ var locations = [
 	['12 Commerford Street', 'Thorold, ON', '$450', 43.1092094, -79.2232908, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/56441942eae37/2800.jpg'), globalImgUrl.concat("/property-types/home.png")], 
 	['234 Summers Drive', 'Thorold, ON', '$400', 43.1114555, -79.2208386, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/56442b50a21ff/nice modern home.jpg'), globalImgUrl.concat("/property-types/home.png")], 
 	['33 Baxter Crescent', 'Thorold, ON', '$500', 43.1103829, -79.2288065, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/56442c1d8edf6/modern pool.jpg'), globalImgUrl.concat("/property-types/home.png")], 
-	['108 Jacobson Road', 'St. Catharines, ON', '$1000', 43.1354108, -79.2275716, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/56444027de74d/house picture.jpg'), globalImgUrl.concat("/property-types/home.png")], 
-	['2533 thorne lodge drive', 'mississauga, ON', '$22', 43.5260902, -79.6738011, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/564441f83b01f/2.PNG'), globalImgUrl.concat("/property-types/home.png")], 
-	['81 Winterberry Blvd', 'Thorold, ON', '$375', 43.1170321, -79.2276325, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/56444826ef0ce/modern pool.jpg'), globalImgUrl.concat("/property-types/home.png")]
+	['46 Mount Forest Drive', 'Brampton, ON', '$123', 43.7329798, -79.8046357, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/56444118a650b/2800.jpg'), globalImgUrl.concat("/property-types/home.png")]
 ];
