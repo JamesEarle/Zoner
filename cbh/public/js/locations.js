293311984310538420e8dc87baad72cb481f2971
Ubuntu <dataAdmin@cbhousing.cbhousing.g3.internal.cloudapp.net>
2015-11-12 08:05:36 +0000

    FINAL REMOTE COMMIT

--- cbh/public/js/locations.js
+++ cbh/public/js/locations.js
@@ -5,5 +5,6 @@ var locations = [
 	['234 Summers Drive', 'Thorold, ON', '$400', 43.1114555, -79.2208386, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/56442b50a21ff/nice modern home.jpg'), globalImgUrl.concat("/property-types/home.png")], 
 	['33 Baxter Crescent', 'Thorold, ON', '$500', 43.1103829, -79.2288065, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/56442c1d8edf6/modern pool.jpg'), globalImgUrl.concat("/property-types/home.png")], 
 	['108 Jacobson Road', 'St. Catharines, ON', '$1000', 43.1354108, -79.2275716, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/56444027de74d/house picture.jpg'), globalImgUrl.concat("/property-types/home.png")], 
-	['2533 thorne lodge drive', 'mississauga, ON', '$22', 43.5260902, -79.6738011, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/564441f83b01f/2.PNG'), globalImgUrl.concat("/property-types/home.png")]
+	['2533 thorne lodge drive', 'mississauga, ON', '$22', 43.5260902, -79.6738011, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/564441f83b01f/2.PNG'), globalImgUrl.concat("/property-types/home.png")], 
+	['81 Winterberry Blvd', 'Thorold, ON', '$375', 43.1170321, -79.2276325, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/56444826ef0ce/modern pool.jpg'), globalImgUrl.concat("/property-types/home.png")]
 ];
