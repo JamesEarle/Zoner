a58864b45edfde7bbca033d572da33ca81c86bfe
Ubuntu <dataAdmin@cbhousing.cbhousing.g3.internal.cloudapp.net>
2015-12-05 05:37:26 +0000

    up to date..again

--- cbh/public/js/locations.js
+++ cbh/public/js/locations.js
@@ -6,4 +6,3 @@ var locations = [
 	['94 Keefer Road', 'Thorold, ON', '$700', 43.1089223, -79.2289621, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/5647a2f81a8e7/IMG_0939.JPG'), globalImgUrl.concat("/property-types/home.png")], 
 	['108 Jabson Street', 'St. Catharines, ON', '$300', 43.1354108, -79.2275716, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/5647b11d010f4/IMG_1008.JPG'), globalImgUrl.concat("/property-types/home.png")], 
 	['123 Fake Street', 'Thorold, ON', '$500', 43.1236091, -79.1989299, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/56480aa13a340/microsoft-hq.jpg'), globalImgUrl.concat("/property-types/home.png")]
-];
