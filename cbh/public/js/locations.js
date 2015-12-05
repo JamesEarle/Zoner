802c3d07aed7db91c5b666b899962240f28ee866
James Earle <j_earle@hotmail.com>
2015-12-05 02:30:13 -0500

    ONE MORE TIME! :+1:

--- cbh/public/js/locations.js
+++ cbh/public/js/locations.js
@@ -1,13 +1,4 @@
 shortGlobalImgUrl = globalImgUrl.substring(0, globalImgUrl.length - 3);
 var locations = [
-<<<<<<< HEAD
 	['63 Commerford St.', 'Thorold, ON', '$450', 43.1096457, -79.2204212, 'http://localhost/Zoner/cbh/public/detail/5662833921fe8', shortGlobalImgUrl.concat('/uploads/5662833921fe8/home.jpg'), globalImgUrl.concat("/property-types/home.png")]
 ];
-=======
-	['12 Commerford Street', 'Thorold, ON', '$450', 43.1092094, -79.2232908, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/56479567a1f22/home.jpg'), globalImgUrl.concat("/property-types/home.png")], 
-	['63 Commerford Street', 'Thorold, ON', '$500', 43.1096457, -79.2204212, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/564797502123a/IMG_2460.JPG'), globalImgUrl.concat("/property-types/home.png")], 
-	['500 Glenridge Ave', 'St. Catharines, ON', '$500', 43.1201777, -79.2467251, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/564798feb532c/deis-architecture.png'), globalImgUrl.concat("/property-types/home.png")], 
-	['94 Keefer Road', 'Thorold, ON', '$700', 43.1089223, -79.2289621, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/5647a2f81a8e7/IMG_0939.JPG'), globalImgUrl.concat("/property-types/home.png")], 
-	['108 Jabson Street', 'St. Catharines, ON', '$300', 43.1354108, -79.2275716, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/5647b11d010f4/IMG_1008.JPG'), globalImgUrl.concat("/property-types/home.png")], 
-	['123 Fake Street', 'Thorold, ON', '$500', 43.1236091, -79.1989299, globalPropertyDetailUrl, shortGlobalImgUrl.concat('/uploads/56480aa13a340/microsoft-hq.jpg'), globalImgUrl.concat("/property-types/home.png")]
->>>>>>> a58864b45edfde7bbca033d572da33ca81c86bfe
